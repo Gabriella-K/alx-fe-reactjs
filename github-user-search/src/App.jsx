27c5090041862ad68b57632be48f3805a2f6d159
Gabriella-K <gabriellaucyeye@gmail.com>
2025-09-21 21:57:39 +0300

    Integrating the GitHub API and creating a Basic Search Feature

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Search from "./components/Search";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <div className="App">
+      <h1>GitHub User Search</h1>
+      <Search />
+    </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
