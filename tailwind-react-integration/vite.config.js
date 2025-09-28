a7496c4616593f50028bad416248adbc6124b571
Gabriella-K <gabriellaucyeye@gmail.com>
2025-09-28 23:41:45 +0300

    Added tailwind-react-integration project with Tailwind setup

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+<<<<<<< HEAD
+=======
+import tailwindcss from '@tailwindcss/vite' 
+>>>>>>> 374d3f5 (Added tailwind-react-integration project with Tailwind setup)
 
 // https://vite.dev/config/
 export default defineConfig({
