69d560165cdb940db8ceff05b1b4cb87ef122b37
Gabriella-K <gabriellaucyeye@gmail.com>
2025-09-28 23:33:04 +0200

    Add files via upload

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,17 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Search from "./components/Search";
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <div className="App">
-      <h1>GitHub User Search</h1>
-      <Search />
-    </div>
+    <UserProfile />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
