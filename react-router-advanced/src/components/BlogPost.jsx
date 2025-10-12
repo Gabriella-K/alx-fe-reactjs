3f9f5a57d61649d974563040c37b99acc2844d4c
Gabriella-K <gabriellaucyeye@gmail.com>
2025-10-13 00:37:56 +0300

    Advanced Routing in React with React Router

--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 const BlogPost = () => {
-  const { postId } = useParams();
+  const { id } = useParams();
   return (
     <div style={{ padding: "20px" }}>
       <h2>Blog Post</h2>
-      <p>Viewing post with ID: {postId}</p>
-      <p>This is a sample blog post content for post {postId}.</p>
+      <p>Viewing post with ID: {id}</p>
+      <p>This is a sample blog post content for post {id}.</p>
     </div>
   );
 };
